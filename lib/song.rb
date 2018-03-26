66c922d436d773b120f5ef96afe5a4b080d4ee0b
Kitty Davies <kittyfdavies@gmail.com>
2018-03-26 19:21:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,21 +2,21 @@ class Song
 
   @@song_count = 0
   @@genre_count = 0
-  
+
   attr_accessor :name, :artist, :genre
-  
+
   def initialize
     @@song_count += 1
   end
-  
+
   def genre_count
     @@genre_count += 1
   end
-  
+
   def self.count
     @@song_count
   end
-  
+
   def self.genre_count
     @@genre_count
   end
